1b71c27c1fce0be3ee6e1b1c90655e7b3c331af6
wmgraves <william.m.graves0@gmail.com>
2022-04-21 17:29:39 -0400

    Test commit

--- main.py
+++ main.py
@@ -5,3 +5,20 @@
 # Description:
 # TODO: add description of this file
 
+import dearpygui.dearpygui as dpg
+
+# Create primary window
+dpg.create_context()
+
+with dpg.window(tag='Primary Window'):
+    dpg.add_text('Test text')
+
+# Finish preparing the window
+dpg.create_viewport(title='Project Tracker')
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window('Primary Window', True)
+
+# Start the window
+dpg.start_dearpygui()
+dpg.destroy_context()
