a91fd43a6507f1f16a0d1cde344228c30a384e99
wmgraves <william.m.graves0@gmail.com>
2022-04-26 14:43:01 -0400

    Minor GUI formatting changes

--- main.py
+++ main.py
@@ -7,26 +7,55 @@
 
 import dearpygui.dearpygui as dpg
 
+# Helper function for centering elements horizontally
+def centerXPos(frameWidth, elementWidth):
+    return frameWidth / 2 - elementWidth / 2
+
 # Initialize variables
+viewportWidth = 620
+viewportHeight = 850
 
+mainMenuWidth = 600
+mainMenuHeight = 800
+mainMenuButtonWidth = 450
+mainMenuElementSpacing = 50
+mainMenuButtonHeight = 30
 
-# Create primary window
+# Prepare images
 dpg.create_context()
-with dpg.window(tag='Primary Window') as window:
-    
-    # Create main menu
-    with dpg.group() as main_menu:
-        dpg.add_text('Title Image Placeholder')
+logoW, logoH, unused, logoData = dpg.load_image('resources/title_image.png')
 
-#     # Create background
-#     with dpg.group(label='Background') as backgroundGroup:
-#         dpg.add_button(label='test button')
+with dpg.texture_registry():
+    dpg.add_static_texture(logoW, logoH, logoData, tag = 'logo')
+
+# Create main menu
+with dpg.window(tag = 'Main Menu', width = mainMenuWidth, height = mainMenuHeight, no_resize = True, no_move = True,
+                no_title_bar = True, no_collapse = True, no_close = True, no_saved_settings = True) as window:
+    # Add logo
+    dpg.add_image('logo', pos = (centerXPos(mainMenuWidth, logoW), mainMenuElementSpacing))
+    
+    # Add menu options
+    with dpg.group(width = mainMenuButtonWidth,
+                   pos = (centerXPos(mainMenuWidth, mainMenuButtonWidth), 2 * mainMenuElementSpacing + logoH)):
+        # Display all existing projects that can be loaded
+        with dpg.child_window(menubar = True, height = 400):
+            with dpg.menu_bar():
+                with dpg.menu(label = 'Load an existing project:'):
+                    pass
+            
+            # Iterate over all saved projects
+            # TODO: Implement project loading feature
+            dpg.add_text('Update this part of the GUI once project data can be saved/loaded')
+        
+        # Display other buttons
+        dpg.add_button(label = 'Create New Project', height = mainMenuButtonHeight)
+        dpg.add_button(label = 'Close Program', height = mainMenuButtonHeight)
 
 # Finish preparing the window
-dpg.create_viewport(title='Project Tracker', width=600, height=800)
+dpg.create_viewport(title = 'Project Tracker', width = viewportWidth, height = viewportHeight)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window('Primary Window', True)
+# dpg.set_primary_window('Primary Window', True)
 
 # Start the window
 dpg.start_dearpygui()
