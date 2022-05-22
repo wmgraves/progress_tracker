2bd0e4b03f87edd0fad61a7e27a5d19c63c1220a
wmgraves <william.m.graves0@gmail.com>
2022-05-22 19:22:05 -0400

    Created file

---
+++ project.py
@@ -0,0 +1,25 @@
+# progress_tracker - project.py
+# Created by: wmgraves (https://github.com/wmgraves)
+# Created on: 05/22/2022
+
+# Description:
+# TODO: add description of this file
+
+# Import external modules
+
+
+# Import custom modules
+
+
+class Project:
+    """
+    Handles all interactions with a project data file
+    """
+
+    def __init__(self, parent, fileName):
+        """
+        Create the Project object and load data from the provided file
+
+        :param parent:
+        :param fileName:
+        """
