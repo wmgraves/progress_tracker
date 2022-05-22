5ac8fa057b22a516f47b5c666ab7e40082a7d51c
wmgraves <william.m.graves0@gmail.com>
2022-05-22 17:58:34 -0400

    Fixed formatting issues

--- main.py
+++ main.py
@@ -6,13 +6,31 @@
 # Starts the app.
 
 # Import external libraries
+import json
+import os
 import wx
 
 # Import custom modules
 import start_frame
 
 
-# Create and start app
+# Create a config file if it does not already exist
+filePath = 'data/settings.json'
+if not (os.path.isfile(filePath) and os.access(filePath, os.R_OK)):
+    print(filePath + ' does not exist, creating file now...')
+    
+    defaultData = {
+        'projects': {
+            'test': False
+        }
+    }
+    
+    with open(filePath, 'w') as settingsFile:
+        settingsFile.write(json.dumps(defaultData))
+    
+    settingsFile.close()
+
+# Create and start the app
 app = wx.App()
 start_frame.StartFrame(None, title = 'Progress Tracker', statusText = 'This app is still in development. Visit '
                                                                       'https://github.com/wmgraves/progress_tracker '
